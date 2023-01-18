a46a857ebc248921aa58ebefe6f36bbeec53c777
Crush-Steelpunch <90606312+Crush-Steelpunch@users.noreply.github.com>
2023-01-18 12:20:32 +0000

    Create buildscript.sh

---
+++ buildscript.sh
@@ -0,0 +1,12 @@
+echo "$MY_VAR_1"
+echo "This is build $BUILD_NUMBER"
+echo "This is build $BUILD_NUMBER" > buildnumberfile.txt
+cat README.md
+cat /etc/os-release
+ls
+touch sheep
+ls
+pwd
+cal
+
+
