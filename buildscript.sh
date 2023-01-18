5a8c143f7831a7aa69044bc84956279a2a2e0d9e
Crush-Steelpunch <90606312+Crush-Steelpunch@users.noreply.github.com>
2023-01-18 14:48:19 +0000

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -3,10 +3,12 @@ echo "This is build $BUILD_NUMBER"
 echo "This is build $BUILD_NUMBER" > buildnumberfile.txt
 cat README.md
 cat /etc/os-release
+mkdir testdir1
 ls
 touch sheep
 ls
 pwd
+mkdir testdir1
 cal
 echo "First argument was $1"
 echo "Second argument was $2"
