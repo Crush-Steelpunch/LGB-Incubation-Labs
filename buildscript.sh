ba7db14ed941c9ca92d92c411e0aae08c1509b19
Crush-Steelpunch <90606312+Crush-Steelpunch@users.noreply.github.com>
2023-01-18 14:57:35 +0000

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,15 +1,19 @@
+EXITVAR=0
 echo "$MY_VAR_1"
 echo "This is build $BUILD_NUMBER"
 echo "This is build $BUILD_NUMBER" > buildnumberfile.txt
 cat README.md
 cat /etc/os-release
 mkdir testdir1
+EXITVAR=$(($EXITVAR + $?))
+
 ls
 touch sheep
 ls
 pwd
 mkdir testdir1
+EXITVAR=$(($EXITVAR + $?))
 cal
 echo "First argument was $1"
 echo "Second argument was $2"
-
+exit $EXITVAR
